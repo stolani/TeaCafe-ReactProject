9c70ea520f222e6d1f4c8061719278d9c857c2ee
stolani <olansiki@gmail.com>
2021-02-07 14:02:03 -0500

    Components and Images

---
+++ src/products.js
@@ -0,0 +1,32 @@
+this.state = {
+    products: [
+        {
+            id: 0,
+            name: 'Fresh Black Tea',
+            image: 'assets/images/black-tea.jpg',
+            price: 2.50
+           
+        },
+        {
+          id: 1,
+          name: 'Oolong Tea',
+          image: 'assets/images/oolong-tea.jpg',
+          price: 3.00
+          
+        },
+        {
+            id: 2,
+            name: 'Croissants',
+            image: 'assets/images/croissants.jpg',
+            price: 1.50
+            
+        },
+        {
+            id: 3,
+            name: 'Apple Pie',
+            image: 'assets/images/tea-pie.jpg',
+            price: 4.00
+        
+        }
+    ],
+};
